c6deb7346c7e7512c23d10dee3c4493d63c96bab
unknown <arslan.farooq_551@hotmail.com>
2020-04-05 03:10:17 -0700

    Work started on pagination.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -12,6 +12,7 @@ export class HomeComponent implements OnInit {
 
   /* Defining posts array to display in the UI */
   posts: Post[];
+  pages: Post[][];
 
   constructor(private postsService: PostsService) { }
 
@@ -21,8 +22,22 @@ export class HomeComponent implements OnInit {
       posts => {
         /* Loading posts in the UI */
         this.posts = posts;
+        /* Creating Pages */
+        this.pages = this.createPages(posts, 10);
+        console.log(this.pages);
       }
     );
+    }
+
+    /* Break the loaded posts array into smaller pages */
+    createPages(loadedPosts: Post[], size: number): Post[][]{
+
+
+      const arrayOfPages = [];
+      for (let i = 0; i < loadedPosts.length; i += size) {
+        arrayOfPages.push(loadedPosts.slice(i, i + size));
+      }
+      return arrayOfPages;
     }
 
 }
